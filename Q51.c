2ce290ff91ab45b76c5cfbf5001725eaf06d36de
Mayank Mudgal <mayankmudgal64@gmail.com>
2025-09-22 13:29:37 +0530

    Q51

---
+++ Q51.c
@@ -0,0 +1,37 @@
+// Q51: Write a program to print the following pattern:
+//     5
+//    45
+//   345
+//  2345
+// 12345
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+    5
+   45
+  345
+ 2345
+12345
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    int i, j, rows = 5;
+
+    for (i = 1; i <= rows; i++) {
+        for (j = 1; j <= rows - i; j++) {
+            printf(" ");
+        }
+        for (j = rows - i + 1; j <= rows; j++) {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
